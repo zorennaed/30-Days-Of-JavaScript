70211d903a1ed55f685e213e41faae830d55987c
annerozed <67812574+zorennaed@users.noreply.github.com>
2023-07-10 10:52:49 +0800

    Exercise-Progress

---
+++ Day-01-Exercise/datatypes.js
@@ -0,0 +1,18 @@
+
+  // comments can make code readable!
+  // Welcome to 30DaysOfJavaScript
+
+  /*
+      Comments can make code readable,
+      easy to reuse and informative.
+  */
+
+  let stringType2 = "30 Days of Javascript";
+  let boolType2 = true;
+  let undefinedType2;
+  let nullType2 = null;
+  
+  console.log(typeof stringType2);
+  console.log(typeof boolType2);
+  console.log(typeof undefinedType2);
+  console.log(typeof nullType2);
---
+++ Day-01-Exercise/variable.js
@@ -0,0 +1,15 @@
+
+  // comments can make code readable!
+  // Welcome to 30DaysOfJavaScript
+
+  /*
+      Comments can make code readable,
+      easy to reuse and informative.
+  */
+
+let stringType = "30 Days of Javascript";
+let boolType = true;
+let undefinedType;
+let nullType = null;
+
+console.log(stringType, boolType, undefinedType, nullType);
